02c6006c1a9aeed2c4084b74ec3d2b9cb92b7737
Luis Gonzalo Morelli <42901084+luisgm21@users.noreply.github.com>
2024-03-05 21:05:24 -0300

    Fin de la Seccion 8

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { LazyImageComponent } from './components/lazy-image/lazy-image.component';
 
 
 
 @NgModule({
-  declarations: [SideBarComponent],
+  declarations: [
+    SideBarComponent,
+    LazyImageComponent
+  ],
   imports: [
     CommonModule
   ],
-  exports:[SideBarComponent]
+  exports:[
+    SideBarComponent,
+    LazyImageComponent
+  ],
 })
 export class SharedModule { }
